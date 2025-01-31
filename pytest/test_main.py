c159798be3dc42e58e6f5c66ca31de598146115b
Dineshkumar S <113044062+dineshsundar30@users.noreply.github.com>
2025-01-31 13:12:10 +0530

    Update test_main.py

--- pytest/test_main.py
+++ pytest/test_main.py
@@ -5,7 +5,7 @@
 #to run all the function file in a directory -->py.test
 #you can run specific file with py.test <filename>   like py.test <filename> -v -s
 # -k stands for method names execution like py.test -k <functionname> -v -s    #--->s to print all counsol logs in out put,  -v stands for verbose to provide more info metadata and here you can pass the partial name for your function name will run all the method which is having  the key word
-# you can mark (tag) tests @pytest.mark.smoke and then run with -m  like py.test -m somke -v -s
+# you can mark (tag) tests @pytest.mark.smoke and then run with -m which is stands for mark like py.test -m somke -v -s
 #you can skip tests with @pytest.mark.skip
 #@pytest.mark.xfail  here it will run but don't show in output
 #fixtures are used as setup and tear down methods for test cases- conftest file to generalize fixt
