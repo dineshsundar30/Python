94b637fd56d0e9d5916062ffa9eb5bbb9ebae288
Dineshkumar S <113044062+dineshsundar30@users.noreply.github.com>
2025-01-31 14:39:56 +0530

    Update conftest.py

--- pytest/conftest.py
+++ pytest/conftest.py
@@ -1,10 +1,10 @@
 import pytest
-
+#if you want to give a scope in class you should create a excat file name with conftest.py then this will fixture will be availabe all pytest file in the specfic folder 
 
 @pytest.fixture(scope="class")
 def setup():
-    print("I will be executing first")
-    yield
+    print("I will be executing first")    #this will run as prerequist steps in function
+    yield                                 # you can define anything that you want to execute after your menthod like closeing browser you can define after this yield keyword
     print(" I will execute last")
 
 
