027aa7b41db1ac21e66a713ac92a22bc6ad1cce8
Dineshkumar S <113044062+dineshsundar30@users.noreply.github.com>
2026-04-19 11:19:29 +0530

    Update Python_LM3_formatting.py

--- Python_LM3_formatting.py
+++ Python_LM3_formatting.py
@@ -90,7 +90,7 @@ O/P:
 or
 rows = 5
 for i in range(1, rows + 1): 
-    for j in range(1, i + 1): 
+    for j in range(i): 
         print("*", end=" ")  
     print()
 ----------------------------------
@@ -113,9 +113,10 @@ or
 
 rows = 5
 for i in range(rows,0, -1): 
-    for j in range(1, i + 1): 
+    for j in range(i): 
         print("*", end=" ")  
     print()
+
 ----------------------------------
 
 
